fb3c971fe37221b990fcf58ac0ba22b3132f5942
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2019-11-22 19:02:24 -0500

    Added missing file

---
+++ fury/test_coords.py
@@ -0,0 +1,85 @@
+import numpy as np
+import numpy.testing as npt
+from fury.utils import vtk, rotate
+from fury import window, utils, actor
+from fury.convert import set_polydata_texture_coords
+
+
+def test_polydata_texture_coords(interactive=False):
+    # Create a cube
+    my_triangles = np.array([[0, 6, 4],
+                             [0, 2, 6],
+                             [0, 3, 2],
+                             [0, 1, 3],
+                             [2, 7, 6],
+                             [2, 3, 7],
+                             [4, 6, 7],
+                             [4, 7, 5],
+                             [0, 4, 5],
+                             [0, 5, 1],
+                             [1, 5, 7],
+                             [1, 7, 3]], dtype='i8')
+    my_vertices = np.array([[0.0, 0.0, 0.0],
+                            [0.0, 0.0, 1.0],
+                            [0.0, 1.0, 0.0],
+                            [0.0, 1.0, 1.0],
+                            [1.0, 0.0, 0.0],
+                            [1.0, 0.0, 1.0],
+                            [1.0, 1.0, 0.0],
+                            [1.0, 1.0, 1.0]])
+    colors = my_vertices * 255
+    my_polydata = vtk.vtkPolyData()
+
+    utils.set_polydata_vertices(my_polydata, my_vertices)
+    utils.set_polydata_triangles(my_polydata, my_triangles)
+
+    # utils.set_polydata_colors(my_polydata, colors)
+    # utils.update_polydata_normals(my_polydata)
+    # normals = utils.get_polydata_normals(my_polydata)
+    # npt.assert_equal(len(normals), len(my_vertices))
+
+    rgb = (55 * np.ones((40, 40, 3))).astype('uint8')
+    rgb[:, :, 0] = 0
+    tex_actor = actor.texture(rgb)
+
+    tcoords = np.array([[0.5, 1., 0.],
+                        [1.5, 2., 0.],
+                        [0.5, 1., 0.],
+                        [1.5, 2., 0.],
+                        [0.5, 1., 0.],
+                        [1.5, 2., 0.],
+                        [0.5, 1., 0.],
+                        [0.5, 1., 0.]], dtype='f4')
+
+    print(tcoords.shape)
+    print(tcoords.dtype)
+    print(tcoords)
+    # print(rgb[:10])
+
+    set_polydata_texture_coords(my_polydata, tcoords)
+    pd_actor = utils.get_actor_from_polydata(my_polydata)
+
+    target_texture = tex_actor.GetTexture()
+    # target_texture.CubeMapOn()
+    # target_texture.PremultipliedAlphaOn()
+    # target_texture.UseSRGBColorSpaceOn()
+    # target_texture.MipmapOn()
+    pd_actor.SetTexture(target_texture)
+    pd_actor.GetMapper().Update()
+    print(target_texture)
+
+    scene = window.Scene()
+    scene.add(pd_actor)
+    #scene.add(tex_actor)
+
+    if interactive:
+        window.show(scene)
+    arr = window.snapshot(scene)
+
+    report = window.analyze_snapshot(arr)
+    npt.assert_equal(report.objects, 1)
+
+
+if __name__ == '__main__':
+    # npt.run_module_suite()
+    test_polydata_texture_coords(True)
